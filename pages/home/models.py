795b15df89a6af2cd8e4239b18e4d675db0f831f
Sudhansu Bhushan Mishra <sbmishra2000@gmail.com>
2022-08-06 13:38:52 +0530

    Fix models

--- pages/home/models.py
+++ pages/home/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Term(models.Model):
-    kanji = models.CharField(150)
-    kana = models.CharField(150)
-    roomaji = models.CharField(150)
-    english_text = models.CharField(150)
+    kanji = models.CharField(max_length=150)
+    kana = models.CharField(max_length=150)
+    roomaji = models.CharField(max_length=150)
+    english_text = models.CharField(max_length=150)
     japanese_audio_path = models.FileField()
     english_audio_path = models.FileField()
 
